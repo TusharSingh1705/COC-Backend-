6ca2c4ca66943bee38fd0fb9080b4e744872a4ca
TusharSingh1705 <tusharsingh1705@gmail.com>
2026-06-04 21:14:27 +0530

    router and controller

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,7 +1,7 @@
-import {router} from "express";
+import {Router} from "express";
 import { registerUser } from "../controllers/user.controller.js";
 
-const router = router();
+const router = Router();
 
 router.route("/register").post(registerUser);
 
